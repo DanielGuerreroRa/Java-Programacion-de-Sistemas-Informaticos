66e52dfb672fba9a62ad960d6fc5bef4effdc6e1
Daniel Guerrero Raez <147421044+DanielGuerreroRa@users.noreply.github.com>
2024-02-27 15:06:00 +0100

    -

--- Validaciones/src/main/java/com/corenetworks/Validaciones/controlador/EmpleadoControlador.java
+++ Validaciones/src/main/java/com/corenetworks/Validaciones/controlador/EmpleadoControlador.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/empleados")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpleadoControlador {
     @Autowired
     private IEmpleadoServicio servicio;
